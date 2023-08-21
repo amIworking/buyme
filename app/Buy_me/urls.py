7a73d50a7891489f158f9ac7cd1ce86298a7d7bd
RchCG <RchChuG@gmail.com>
2023-08-21 04:10:59 +0300

    Added JWT auth.

--- app/Buy_me/urls.py
+++ app/Buy_me/urls.py
@@ -15,14 +15,16 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, re_path, include
+from django.urls import path
+from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView, TokenVerifyView
 
 from apps.shop.views import ShopApiDetailView, ShopApiList
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('api/v1/auth/', include('djoser.urls')),
-    re_path(r'^auth/', include('djoser.urls.authtoken')),
+    path('api/v1/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('api/v1/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+    path('api/v1/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path("api/v1/shopslist", ShopApiList.as_view()),
     path("api/v1/shopsdetail/<int:pk>/", ShopApiDetailView.as_view()),
 ]
